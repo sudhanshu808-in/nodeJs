c9ad594401f369cc8c9273778afc08f21ae9e1e3
Sudhanshu <shudhanshukaprowan@gmail.com>
2024-07-05 11:06:06 +0530

    export/import mdoules

--- starting/hello.js
+++ starting/hello.js
@@ -1,1 +1,3 @@
-console.log("sudhanshu kaprowan");
+const math= require('./math')
+console.log(math.sub(10,6));
+console.log("sudhanshu kaprowan"); 
---
+++ starting/math.js
@@ -0,0 +1,14 @@
+function  add(a,b){
+  return a+b;
+}
+function sub(a,b){
+    return a-b;
+}
+module.exports= {
+    "add":add,
+     "sub":sub};
+// exports.add1(a,b)=(a,b)=>{
+//     return a+b;
+// }
+// exports.sub1=(a,b)=>{
+//  return a-b;}

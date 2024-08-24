1c0b5cf1e03c8cd1d10206c07a7b217b7b1b63e0
Sudhanshu <shudhanshukaprowan@gmail.com>
2024-08-25 00:04:56 +0530

    cookies

--- express/Express.js
+++ express/Express.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json);
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", function(req,res){
     res.send("I is am the one");
 })

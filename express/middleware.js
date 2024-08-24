d6e84a1c5b9ff274759c655f635805d70c9ac6ff
Sudhanshu <shudhanshukaprowan@gmail.com>
2024-08-24 23:57:11 +0530

    cookies update

--- express/middleware.js
+++ express/middleware.js
@@ -9,7 +9,17 @@ app.use(function(req,res,next){
 
 
 app.get('/',function(req,res){
-    res.send('Hello World!');
+    res.send('SK Phenomenols');
+})
+
+app.get('/profiles',function(req,res,next){
+    // res.send('SK Phenomenols');
+    return next(new Error('Something went wrong'))
+})
+
+app.use(function(err,req,res,next){
+    console.error(err.stack);
+    res.status(500).send('Something broke !'); // ye error jayga fornt end p kyuki response h 
 })
 
 app.listen(3000)

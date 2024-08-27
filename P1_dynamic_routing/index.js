909871a404d6b7374bdad834a3e01f0239500845
Sudhanshu <shudhanshukaprowan@gmail.com>
2024-08-27 21:35:44 +0530

    dynamic_routing

---
+++ P1_dynamic_routing/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path'); // Import the path module
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');// helps in rendering ejs pages
+app.use(express.static(path.join(__dirname, 'public'))); // Use forward slashes for compatibility
+
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+app.get("/profiles/:username", function(req, res) {
+    // res.render("index");
+    // res.render("index");
+    res.send("Hello," + req.params.username);
+});
+
+app.get("/profiles/:username/:age", function(req, res) {
+    // res.render("index");
+    // res.render("index");
+    res.send(req.params.age);
+});
+
+app.listen(3000, function() {
+    console.log('Server is running on port 3000');
+});
--- express/Express.js
+++ express/Express.js
@@ -5,7 +5,7 @@ app.use(express.json);
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", function(req,res){
-    res.send("I is am the one");
+    res.send("I is am the one"); 
 })
 
 app.get("/shubham", function(req,res){

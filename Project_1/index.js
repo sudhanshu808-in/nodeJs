82ad12d40315176ba5ec4143fb0a3c02abc2f9e1
Sudhanshu <shudhanshukaprowan@gmail.com>
2024-08-30 15:38:31 +0530

    feature updates

--- Project_1/index.js
+++ Project_1/index.js
@@ -22,5 +22,15 @@ res.redirect("/");
 });
 })
 
+// utf-8 is important  as it converts the data into english readable format.... otherwise it will be in buffer form 
+app.get("/file/:filename",function(req,res){
+   fs.readFile(`./files/${req.params.filename}`,"utf-8",function(err,filedata){
+    res.render("show",{filename : req.params.filename , filedata : filedata});
+   });
+})
+ // yaha pr  data aega through form submition AS  the form gets submitted wo yaha par aega sara data fir  yaha se with the help of req.body.title / details hum wo data fetch  krenngey 
+
+// How req.body Works
+// When a client submits a form or sends data to the server via an HTTP POST or PUT request, the data is included in the body of the request. Express.js, with the help of middleware, parses this data and makes it available through the req.body object.
 
 app.listen(3000);

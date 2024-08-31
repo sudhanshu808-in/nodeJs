7c9125d2c245fbfd6e9839e89f4b121084bdaf96
Sudhanshu <shudhanshukaprowan@gmail.com>
2024-08-31 08:21:59 +0530

    project_1 updates

--- Project_1/index.js
+++ Project_1/index.js
@@ -28,9 +28,17 @@ app.get("/file/:filename",function(req,res){
     res.render("show",{filename : req.params.filename , filedata : filedata});
    });
 })
- // yaha pr  data aega through form submition AS  the form gets submitted wo yaha par aega sara data fir  yaha se with the help of req.body.title / details hum wo data fetch  krenngey 
+// yaha pr  data aega through form submition AS  the form gets submitted wo yaha par aega sara data fir  yaha se with the help of req.body.title / details hum wo data fetch  krenngey 
 
 // How req.body Works
 // When a client submits a form or sends data to the server via an HTTP POST or PUT request, the data is included in the body of the request. Express.js, with the help of middleware, parses this data and makes it available through the req.body object.
+app.get("/edit/:filename",function(req,res){
+   res.render("edit",{filename : req.params.filename});
+ })
 
+app.post("/edit",function(req,res){
+   fs.rename(`./files/${req.body.previous}`,`./files/${req.body.newname}`,function(err){
+    res.redirect("/");
+   })
+    })
 app.listen(3000);

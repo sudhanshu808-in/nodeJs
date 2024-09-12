a4fe8e620f0196d43b015d53384767aa8c60fc54
Sudhanshu <shudhanshukaprowan@gmail.com>
2024-09-12 13:24:00 +0530

    authentication

--- authentication/app.js
+++ authentication/app.js
@@ -51,13 +51,17 @@ app.get("/login",(req,res)=>{
 app.post("/login", async(req,res)=>{
     //     email:: This is the key of the object that represents the field name in your MongoDB collection.
     // req.body.email: This is the dynamic value you want to match against the email field in the collection.
+    const {email,password}=req.body
+    if(!email || !password) res.send("Something went wrong");
     let user = await userModel.findOne({email : req.body.email}); 
      if(!user){
         res.send("SOMETHING WENT WRONG")
      }
      bcrypt.compare(req.body.password,user.password,(err,result)=>{
         if(result){
-            res.send("LOGEED in ")}
+            res.send("LOGEED in ")
+         let token = jwt.sign({email : user.email},"shshhhhhhhhhhhhhhhh");
+        }
         else{
             res.send("SOMETHING IS WRONG")
         }

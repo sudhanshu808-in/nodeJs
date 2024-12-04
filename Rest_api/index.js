88c8e4ac55aab40f575958d349fdb774dd71acd2
sudhanshu808-in <shudhanshukaprowan@gmail.com>
2024-12-04 18:50:25 +0530

    update

--- Rest_api/index.js
+++ Rest_api/index.js
@@ -12,48 +12,51 @@ next();
 })
 
 app.get("/",(req,res)=>{
+console.log(req.body);
+
     res.send("home page");
+
 })
 
-app.get("/api/users",(req,res)=>{
-    res.send(users);
-})
+// app.get("/api/users",(req,res)=>{
+//     res.send(users);
+// })
 
-app.get("/users",(req,res)=>{
-    const html = `
-    <ul>
-    ${users.map(user=>`<li>${user.first_name}</li>`).join("")}
-    </ul>
-    `;
-    res.send(html);
+// app.get("/users",(req,res)=>{
+//     const html = `
+//     <ul>
+//     ${users.map(user=>`<li>${user.first_name}</li>`).join("")}
+//     </ul>
+//     `;
+//     res.send(html);
 
     
-});
+// });
 
-app.get("/users/:id",(req,res)=>{
-    const data = Number( req.params.id);
-    const user = users.find(user=>user.id ===data);
-    if(!user){
-        res.status(404).send("user not found");
-    }
-    res.send(user);
-})
+// app.get("/users/:id",(req,res)=>{
+//     const data = Number( req.params.id);
+//     const user = users.find(user=>user.id ===data);
+//     if(!user){
+//         res.status(404).send("user not found");
+//     }
+//     res.send(user);
+// })
 
-app.post("/users/name/:user",(req,res)=>{
-   res.send("pending.......");
+// app.post("/api/users",(req,res)=>{
+// return res.json({status : " pending"})
     
-})
+// })
 
-app.patch("/users/name/:user",(req,res)=>{
-    res.send("pending.......");
-     
- })
+// app.patch("/api/users/:id",(req,res)=>{
+//     return res.json({status : " pending"});
+
+//  })
 
  
-app.delete("/users/name/:user",(req,res)=>{
-    res.send("pending.......");
+// app.delete("/users/name/:user",(req,res)=>{
+//     res.send("pending.......");
      
- })
+//  })
 
 // app.route("/api/user/:id").get((req,res)=>{
 //     const data = Number( req.params.id);
